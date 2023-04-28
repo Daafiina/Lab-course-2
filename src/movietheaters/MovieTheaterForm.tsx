87a2a4ebeba7cabc2e1625b372ff1adb8818850d
Daafiina <da52811@ubt-uni.net>
2023-04-29 01:10:22 +0200

    Map function added

---
+++ src/movietheaters/MovieTheaterForm.tsx
@@ -0,0 +1,39 @@
+import { Form, Formik, FormikHelpers } from "formik";
+import TextField from "../forms/TextField";
+import Button from "../utilis/Button";
+import Map from "../utilis/Map";
+import { Link } from "react-router-dom";
+import { movieTheaterCreationDTO } from "./movieTheater.model";
+import * as  Yup from 'yup';
+
+export default function MovieTheaterForm(props:movieTheaterForm){
+    return(
+        <Formik
+            initialValues={props.model}
+            onSubmit={props.onSubmit}
+            validationSchema={Yup.object({
+                name:Yup.string().required('This field is required').firstLetterUppercase(),
+
+            })}>
+            {(formikProps)=>(
+                <Form>
+                    <TextField displayName="Name" field="name"/>
+                    <div style={{marginBottom:'1rem'}}>
+                        <Map/>
+                    </div>
+
+                    <Button disabled={formikProps.isSubmitting} type="submit">
+                        Save Changes</Button>
+                        <Link className="btn btn-secondary" to="/movietheatres">Cancel</Link>
+                </Form>
+            )}
+        </Formik>
+    )
+
+}
+
+
+interface movieTheaterForm{
+    model:movieTheaterCreationDTO;
+    onSubmit(values:movieTheaterCreationDTO,actions:FormikHelpers<movieTheaterCreationDTO>):void;
+}
